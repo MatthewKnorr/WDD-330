c03251ea46c990241abf30705e8f8c02cb2656f1
MattheKnorr <matt@knorrs.net>
2026-01-24 19:21:36 -0700

    Fixed error for page handling

--- src/js/product.js
+++ src/js/product.js
@@ -1,6 +1,6 @@
-import { getParam } from './utils.mjs';
-import productDetails from './productDetails.mjs';
+import { getParam } from "./utils.mjs";
+import productDetails from "./productDetails.mjs";
 
-const productId = getParam('product');
+const productId = getParam("product");
 // console.log(findProductById(productId));
 productDetails(productId);
