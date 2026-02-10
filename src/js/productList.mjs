c8499535453dfcbf915db3650a0489e137e371a6
sarahdombroski <dom22003@byui.edu>
2026-02-10 16:18:22 -0700

    finished quick lookup

--- src/js/productList.mjs
+++ src/js/productList.mjs
@@ -1,4 +1,4 @@
-import { getData } from './productData.mjs';
+import { getData, findProductById } from './productData.mjs';
 import { renderListWithTemplate } from './utils.mjs';
 import { discountPercent} from './utils.mjs';
 
@@ -12,11 +12,51 @@ function productCard(product){
             <h3 class="card__brand">${product.Brand.Name}</h3>
             <h2 class="card__name">${product.NameWithoutBrand}</h2>
 
+            <button class="quick-view" id=${product.Id}>Quick View</button>
+
             <p class="product-card__price">
             <span class = "strikethrough"> $${product.SuggestedRetailPrice}</span> $${product.FinalPrice}
             <span class = "percent-off"> Save ${discountPercent(product.SuggestedRetailPrice, product.FinalPrice)}%</p>
             </a>
           </li>`
+}
+
+function productModal(product) {
+  return `
+    <dialog class="product-detail">
+        <button class="closeModal">X</button>
+        <h3 id="productName">${product.Brand.Name}</h3>
+
+        <h2 class="divider" id="productNameWithoutBrand">${product.NameWithoutBrand}</h2>
+
+        <img class="divider" src="${product.Image}" alt="${product.Name}" id="productImage" />
+
+        <p class="product-card__price" id="productPrice">
+          <span class = "strikethrough">$${product.SuggestedRetailPrice}</span> $${product.FinalPrice}
+          <span class = "percent-off">${discountPercent(product.SuggestedRetailPrice, product.FinalPrice)}% Off!
+        </p>
+
+        <p class="product__color" id="productColor">${product.Colors[0].ColorName}</p>
+
+        <p class="product__description" id="productDescription">${product.DescriptionHtmlSimple}</p>
+
+        <div class="product-detail__add">
+          <button id="addToCart" data-id="${product.Id}">Add to Cart</button>
+        </div>
+      </dialog>
+  `
+}
+
+function renderModal(product) {
+  const mainEl = document.querySelector('main');
+  mainEl.insertAdjacentHTML('beforeend', productModal(product));
+  const dialog = document.querySelector('dialog');
+  dialog.showModal();
+
+  const close = document.querySelector('.closeModal');
+  close.addEventListener('click', () => {
+    dialog.close();
+  })
 }
 
 export async function productList(selector, category){
@@ -26,6 +66,16 @@ export async function productList(selector, category){
 
   // Rendering
   renderListWithTemplate(productCard, el, products)
+
+  // Set up buttons
+  const buttons = document.querySelectorAll('.quick-view');
+  buttons.forEach(button => {
+    button.addEventListener('click', async () => {
+      event.preventDefault();
+      const product = await findProductById(button.id);
+      renderModal(product);
+    })
+  })
 
   /*
   console.log(products);
