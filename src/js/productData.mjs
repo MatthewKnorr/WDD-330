846239adf3e36100cd9be4208a518eae081a624d
MattheKnorr <matt@knorrs.net>
2026-02-11 17:05:38 -0700

    Netlify bug fix

--- src/js/productData.mjs
+++ src/js/productData.mjs
@@ -9,7 +9,7 @@ function convertToJson(res) {
 }
 
 export async function getData(category) {
-  const response = await fetch(baseURL + `products/search/${category}`);
+  const response = await fetch(baseURL + `/products/search/${category}`);
   const data = await convertToJson(response);
   return data.Result;
 }
@@ -21,7 +21,7 @@ export async function getJson(json) {
 }
 
 export async function findProductById(id) {
-  const response = await fetch(baseURL + `product/${id}`);
+  const response = await fetch(baseURL + `/product/${id}`);
   const product = await convertToJson(response);
   return product.Result;
 }
